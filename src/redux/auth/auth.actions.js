6c597db88d39333c02dcb52f59a0a7c8eb83afd1
RitvijSrivastava <ritvijsrivastava99@gmail.com>
2021-05-26 15:08:10 +0530

    :tada: Initial Commit

---
+++ src/redux/auth/auth.actions.js
@@ -0,0 +1,146 @@
+"use strict";
+
+import axios from "axios";
+import { API } from "../../backend";
+import {
+  SIGN_UP_REQUEST,
+  SIGN_IN_REQUEST,
+  SIGN_OUT_REQUEST,
+  SIGN_UP_SUCCESS,
+  SIGN_IN_SUCCESS,
+  SIGN_OUT_SUCCESS,
+  SIGN_UP_FAILURE,
+  SIGN_IN_FAILURE,
+  SIGN_OUT_FAILURE,
+} from "./auth.types";
+
+const signUpRequest = () => {
+  return {
+    type: SIGN_UP_REQUEST,
+  };
+};
+
+const signInRequest = () => {
+  return {
+    type: SIGN_IN_REQUEST,
+  };
+};
+
+const signOutRequest = () => {
+  return {
+    type: SIGN_OUT_REQUEST,
+  };
+};
+
+const signUpSuccess = () => {
+  return {
+    type: SIGN_UP_SUCCESS,
+  };
+};
+
+const signInSuccess = (user) => {
+  return {
+    type: SIGN_IN_SUCCESS,
+    payload: user,
+  };
+};
+
+const signOutSuccess = () => {
+  return {
+    type: SIGN_OUT_SUCCESS,
+  };
+};
+
+const signUpFailure = (errorMsg) => {
+  return {
+    type: SIGN_UP_FAILURE,
+    payload: errorMsg,
+  };
+};
+
+const signInFailure = (errorMsg) => {
+  return {
+    type: SIGN_IN_FAILURE,
+    payload: errorMsg,
+  };
+};
+
+const signOutFailure = (errorMsg) => {
+  return {
+    type: SIGN_OUT_FAILURE,
+    payload: errorMsg,
+  };
+};
+
+export const signUpUser = (user) => {
+  return (dispatch) => {
+    dispatch(signUpRequest());
+    axios
+      .post(`${API}/auth/signup`, JSON.stringify(user), {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        const data = response.data;
+        if (data.error) {
+          dispatch(signUpFailure(data.error));
+        } else {
+          dispatch(signUpSuccess());
+        }
+      })
+      .catch((error) => {
+        const errorMsg = error.response.data.error;
+        dispatch(signUpFailure(errorMsg));
+      });
+  };
+};
+
+export const signInUser = (user) => {
+  return (dispatch) => {
+    dispatch(signInRequest());
+    axios
+      .post(`${API}/auth/signin`, JSON.stringify(user), {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        const data = response.data;
+        if (data.error) {
+          dispatch(signInFailure(data.error));
+        } else {
+          dispatch(signInSuccess(data.user));
+        }
+      })
+      .catch((error) => {
+        const errorMsg = error.response.data.error;
+        dispatch(signInFailure(errorMsg));
+      });
+  };
+};
+
+export const sigOutUser = () => {
+  return (dispatch) => {
+    dispatch(signOutRequest());
+    axios.get(`${API}/auth/signout`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }),
+      then((response) => {
+        const data = response.data;
+        if (data.error) {
+          dispatch(signOutFailure(data.error));
+        } else {
+          dispatch(signOutSuccess());
+        }
+      }).catch((error) => {
+        const errorMsg = error.response.data.error;
+        dispatch(signOutFailure(errorMsg));
+      });
+  };
+};
---
+++ src/redux/auth/auth.reducer.js
@@ -0,0 +1,61 @@
+"use strict";
+
+import {
+  SIGN_UP_REQUEST,
+  SIGN_IN_REQUEST,
+  SIGN_OUT_REQUEST,
+  SIGN_UP_SUCCESS,
+  SIGN_IN_SUCCESS,
+  SIGN_OUT_SUCCESS,
+  SIGN_UP_FAILURE,
+  SIGN_IN_FAILURE,
+  SIGN_OUT_FAILURE,
+} from "./auth.types";
+
+const initialState = {
+  loading: false,
+  user: null,
+  error: "",
+};
+
+export const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SIGN_UP_REQUEST:
+    case SIGN_IN_REQUEST:
+    case SIGN_OUT_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        user: null,
+        error: "",
+      };
+    case SIGN_UP_SUCCESS:
+    case SIGN_OUT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        user: null,
+        error: "",
+      };
+    case SIGN_IN_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        user: action.payload,
+        error: "",
+      };
+    case SIGN_UP_FAILURE:
+    case SIGN_IN_FAILURE:
+    case SIGN_OUT_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        user: null,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
